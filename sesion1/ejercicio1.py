bad8e6620d1776055607de828555bfda5f4ce64a
Replit user <>
2022-10-07 19:12:09 +0000

    Ejercicio de la primera sesión

---
+++ sesion1/ejercicio1.py
@@ -0,0 +1,35 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion/ejercicio1.py
+Autor: Programador x
+Action: asignación de variables
+'''
+nombre_estado = 'Tabasco'
+superficie = 25267
+poblacion_total = 2403000000
+promedio_temperatura = 31.3
+estados_cercanos = ['chiapas', 'campeche', 'veracruz']
+productos = ['cacao', 'coco', 'caña']
+
+print(
+  nombre_estado,
+  " es un estado que ",
+)
+print("con ", estados_cercanos[0], "colinda al sur", "y")
+
+print()
+
+print("Tiene una superficie de: ", superficie)
+
+print()
+
+print("Tabasco cuenta con una población total de: ", poblacion_total)
+
+print()
+
+print("Su temperatura promedio es de: ", promedio_temperatura)
+
+print()
+
+print("Producen productos como el: ", productos)
